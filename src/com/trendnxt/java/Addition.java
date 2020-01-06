ecbaceafdc8df672e9f9ed0c01c67b94e93fd1dd
unknown <joydeep127@gmail.com>
2020-01-06 14:26:39 +0530

    Initial Commit

---
+++ src/com/trendnxt/java/Addition.java
@@ -0,0 +1,8 @@
+package com.trendnxt.java;
+
+public class Addition {
+	void fnAdd(int iFirst, int iSecond){
+		int iRes = iFirst + iSecond;     
+		System.out.println("The output is " + iRes);
+	}
+}
---
+++ src/com/trendnxt/java/ArithmeticOperation.java
@@ -0,0 +1,8 @@
+package com.trendnxt.java;
+
+public class ArithmeticOperation {
+	public static void main (String args[]) {
+		Addition objAddition= new Addition();
+		objAddition.fnAdd(3,9);
+	}
+}
